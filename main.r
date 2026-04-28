efb8438ab251210a3db3245c410521a1b7a87257
Gatasso <galassoworkstation@gmail.com>
2026-04-28 17:55:21 -0300

    ajusta main.r, transformando-o em execução única

--- main.r
+++ main.r
@@ -1,20 +1,35 @@
+# Configurações de Caminho
 path_directory <- setwd("C:/Users/Pesquisa/Documents/IC2026-MGR")
-path_scripts <- paste0(path_directory,"/scripts")
-path_run <- paste0(path_directory, "/search_runs/")
+path_scripts   <- paste0(path_directory, "/scripts")
+path_run       <- paste0(path_directory, "/search_runs/")
 
-source(paste0(path_scripts,"/00_init_libraries.r"))
-source(paste0(path_scripts,"/01_import_raw_files.r"))
-source(paste0(path_scripts,"/02_convert_files.r"))
-source(paste0(path_scripts,"/03_clean_data.r"))
-source(paste0(path_scripts,"/04_export_converted_files.r"))
+# Carrega as definições das funções (Source) de uma só vez
+invisible(lapply(list.files(path_scripts, pattern = "\\.[Rr]$|\\.r$", full.names = TRUE), source))
 
-RUN_PATH <- paste0(path_run, "02_270426/")
+# --- PREENCHIMENTO MANUAL ---
+RUN_FOLDER <- "02_270426/" 
+# ----------------------------
 
-initialize_libraries()
-run_scripts()
-data_raw_imported <- import_data(RUN_PATH)
-data_converted <- convert_data(data_raw_imported, "bibtex")
-data_cleaned <- clean_and_merge_data(data_converted)
-export_bib_data(data_cleaned, RUN_PATH)
+run_bibliometrix_pipeline <- function(folder_name) {
+  target_path <- paste0(path_run, folder_name)
+  
+  message("________Iniciando Pipeline________")
+  
+  initialize_libraries()
+  
+  # Fluxo de Dados
+  data_raw       <- import_data(target_path)
+  data_converted <- convert_data(data_raw, "bibtex")
+  data_cleaned   <- clean_and_merge_data(data_converted)
+  
+  # Exportação dos Arquivos
+  export_bib_data(data_cleaned, target_path)
+  
+  message("________Pipeline concluído________")
+  
+  # Abre a interface do Biblioshiny
+  biblioshiny()
+}
 
-initialize_biblioshiny()
+# Execução Única
+run_bibliometrix_pipeline(RUN_FOLDER)
